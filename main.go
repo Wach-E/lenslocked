6f4557be6fbb5d22a34b6e6f49ff0a51ad639eed
Wachukwu Emmanuel <immacurte1@gmail.com>
2022-05-08 12:16:38 +0100

    Added new pages and custom routes

--- main.go
+++ main.go
@@ -5,12 +5,65 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Welcome to my first awesome website!</h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:immacurte1@gmail.com\">Wach Email</a>.</p>")
+}
+
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `<h1>FAQ Page</h1>
+  <ul>
+	<li>
+	  <b>Is there a free version?</b>
+	  Yes! We offer a free trial for 30 days on any paid plans.
+	</li>
+	<li>
+	  <b>What are your support hours?</b>
+	  We have support staff answering emails 24/7, though response
+	  times may be a bit slower on weekends.
+	</li>
+	<li>
+	  <b>How do I contact support?</b>
+	  Email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com</a>
+	</li>
+  </ul>
+  `)
+}
+
+// func pathHandler(w http.ResponseWriter, r *http.Request) {
+// 	switch r.URL.Path {
+// 	case "/":
+// 		homeHandler(w, r)
+// 	case "/contact":
+// 		contactHandler(w, r)
+// 	default:
+// 		http.Error(w, "Page Not Found", http.StatusNotFound)
+// 	}
+// }
+
+type Router struct{}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
+	default:
+		http.Error(w, "Page not found", http.StatusNotFound)
+	}
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	var router Router
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", router)
 }
