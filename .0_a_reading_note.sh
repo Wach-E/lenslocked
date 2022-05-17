bd7c43e6fbf15491ff292b17ca7735b6b35b6848
Wachukwu Emmanuel <immacurte1@gmail.com>
2022-05-17 21:42:40 +0100

    added context tutorial

--- .0_a_reading_note.sh
+++ .0_a_reading_note.sh
@@ -1,3 +1,5 @@
-Reference to Golang basics: https://www.geeksforgeeks.org/golang/
+- Reference to Golang basics: https://www.geeksforgeeks.org/golang/
 
-Structs and Functions: https://www.bogotobogo.com/GoLang/GoLang_Structs.php
+- Structs and Functions: https://www.bogotobogo.com/GoLang/GoLang_Structs.php
+
+- Contexts in GO: https://www.digitalocean.com/community/tutorials/how-to-use-contexts-in-go
