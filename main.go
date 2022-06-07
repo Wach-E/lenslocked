85e8d7c9a97b04b1d6ada259c9206341c53f6e6f
Wachukwu Emmanuel <immacurte1@gmail.com>
2022-06-07 12:25:08 +0100

    Refactored home handler to use html template for client side rendering

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -11,7 +14,22 @@ import (
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Welcome to my first awesome website!</h1>")
+	tplPath := filepath.Join("templates", "home.gohtml")
+	// tpl, err := template.ParseFiles("templates\\home.gohtml")
+	tpl, err := template.ParseFiles(tplPath)
+	if err != nil {
+		log.Printf("parsinng template: %v", err)
+		http.Error(w, "There was an error parsing the template.",
+			http.StatusInternalServerError)
+		return
+
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		log.Printf("executing template: %v", err)
+		http.Error(w, "There was an error executing the template.",
+			http.StatusInternalServerError)
+		return
+	}
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
